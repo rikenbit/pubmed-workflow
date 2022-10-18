ec9ee1be484e1279efc0d61ee60d0fbfbb6a6356
Manabu ISHII <manabu.ishii.rb@gmail.com>
2022-10-18 13:35:42 +0900

    Fix upload files
    include tibble/, datatable/ (and sqlite/)

--- src/upload_prepare_data_AHPubMedDbs.sh
+++ src/upload_prepare_data_AHPubMedDbs.sh
@@ -5,4 +5,6 @@ OUTPUT=$2
 
 mkdir -p Azure/AHPubMedDbs/${METADATA_VERSION}
 cp -rf sqlite/*  Azure/AHPubMedDbs/${METADATA_VERSION}/
+cp -rf tibble/*  Azure/AHPubMedDbs/${METADATA_VERSION}/
+cp -rf datatable/*  Azure/AHPubMedDbs/${METADATA_VERSION}/
 echo "" > ${OUTPUT}
