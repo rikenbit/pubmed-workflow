7cb436a871c22e02497bba6e99c121889b1333a0
Manabu ISHII <manabu.ishii.rb@gmail.com>
2024-09-13 14:27:17 +0900

    Update handling newline for abstract

--- src/preprocess_sqlite_abstract.py
+++ src/preprocess_sqlite_abstract.py
@@ -8,7 +8,7 @@ db_connection = sqlite3.connect("sqlite/abstract.sqlite")
 db_cursor = db_connection.cursor()
 db_cursor.execute("CREATE TABLE abstract (pmid INTEGER, journal TEXT, year INTEGER, title TEXT, publicationtype TEXT, abstract TEXT);")
 db_cursor.execute("BEGIN TRANSACTION")
-with open(import_file_name, 'r') as import_file:
+with open(import_file_name, 'r', newline='\n') as import_file:
 	for line in import_file:
 		cleaned_columns = [column.strip() for column in line.split(cell_separator)]
 		db_cursor.execute(insert_query, tuple(cleaned_columns))
